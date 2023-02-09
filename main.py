fde5d55728831708e9574947f8589874d8214bc7
Calin Mihai <32879010+CalinMihai@users.noreply.github.com>
2023-02-09 15:58:58 +0000

    Implemented zen quotes responses from zenquotes.io, and encouragement responses on key sad wording.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
 import discord
+import requests
+import json
+import random
+
 
 intent = discord.Intents.default()
 intent.members = True
@@ -7,6 +11,20 @@ intent.message_content = True
 
 client = discord.Client(intents=intent)
 my_secret = os.environ['TOKEN']
+
+sad_words = ["sad", "depressed", "depressing", "miserable"]
+starter_encouragements = [
+  "Cheer up!",
+  "Hang in there.",
+  "You'll be fine!",
+  "Will you quit winning?"
+]
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = "\"" + json_data[0]['q'] + "\"" + " by " + json_data[0]['a']
+  return (quote)
 
 @client.event
 async def on_ready():
@@ -18,8 +36,11 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith("$hello"):
-    await message.channel.send("Hello!")
-
+  if message.content.startswith("$inspire"):
+    quote = get_quote()
+    await message.channel.send(quote)
+    
+  if any (word in message.content for word in sad_words):
+    await message.channel.send(random.choice(starter_encouragements))
 
 client.run(os.getenv('TOKEN'))
